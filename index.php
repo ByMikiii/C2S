76230e9246e65b4f30d3f430f43394c8bb066480
ByMikiii <mikixr1@gmail.com>
2022-04-15 00:44:18 +0200

    Hashtags repeat fix 😐

--- index.php
+++ index.php
@@ -14,21 +14,22 @@ if(isset($_SESSION['username'])) : ?>
 </form>
   <?php 
   for($i = 0; $i < 8; $i++){
-  echo '<p class="hashtag">';
-  $inputHashtags = ['#gaming', '#czsk', '#funny', '#twitchstreamer', '#tiktok', '#foryoupage', '#fyp', '#foryou', '#viral', '#love', '#funny', '#memes', '#followme', '#fail', '#trending', '#featureme', ' #lifestyle', '#beautiful', '#explore', '#comedy'];
-  $outputHashtags = '';
-  while(strlen($outputHashtags) <= 130){
-   $index = rand(0, count($inputHashtags)- 1);
-   if(str_contains($outputHashtags, $inputHashtags[$index])){
-    $outputHashtags .= $inputHashtags[$index] . " ";
+    echo '<p class="hashtag">';
+    $inputHashtags = ['#gaming', '#czsk', '#funny', '#twitchstreamer', '#tiktok', '#foryoupage', '#fyp', '#foryou', '#viral', '#love', '#funny', '#memes', '#followme', '#fail', '#trending', '#featureme', ' #lifestyle', '#beautiful', '#explore', '#comedy'];
+    $outputHashtags = '';
+    while(strlen($outputHashtags) <= 130){
+     $index = rand(0, count($inputHashtags)- 1);
+     if (!str_contains($outputHashtags, $inputHashtags[$index])){
+      $outputHashtags .= $inputHashtags[$index] . " ";
+    }
+    }
+    echo $outputHashtags;
+    echo '</p>';
   }
-  }
-  echo $outputHashtags;
-  echo '</p>';
-}
-  ?>
 
-<?php endif; ?>
-<?php
-require('./parts/footer.php');
-?>
+endif;
+    ?>
+  
+  <?php
+  require('./parts/footer.php');
+  ?>
