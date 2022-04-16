4aca649759df51a753973eccfee3d654f20d4222
ByMikiii <mikixr1@gmail.com>
2022-04-16 14:24:00 +0200

    New Clip Format Workin 😗

--- phpscripts/urlscript.php
+++ phpscripts/urlscript.php
@@ -6,8 +6,13 @@ if(strpos($clipurl, "?") !== false){
 	$finalclip = explode("?", $clipurl);
 	$clipurl = $finalclip[0];
 }
+if(strpos($clipurl, "clips.twitch.tv")){
 $finalclip = explode("/", $clipurl);
-  $clipurl = $finalclip[5]; 
+  $clipurl = $finalclip[3]; 
+}else{
+  $finalclip = explode("/", $clipurl);
+    $clipurl = $finalclip[5]; 
+  }
   header('Location: https://streamladder.com/twitch-clip/'.$clipurl.'/templates/blurredbackground');
 ?>
 
